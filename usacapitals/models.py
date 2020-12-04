333d9d2265333206d48248486f2f0e6a8bd97e7b
butterfly1of4 <jhouck29@gmail.com>
2020-12-04 13:52:28 -0500

    working through

--- usacapitals/models.py
+++ usacapitals/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Capitals(models.Model):
+    state = models.CharField(max_length=150, default="state")
+    captial = models.CharField(max_length=200, default="capital")
+    
+    def __str__(self):
+        return self.state
