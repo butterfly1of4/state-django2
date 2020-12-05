2659cb6454d6cf626bd02fee77e6e29f77cfcc1b
butterfly1of4 <jhouck29@gmail.com>
2020-12-05 14:28:46 -0500

    rendered list

--- usacapitals/views.py
+++ usacapitals/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Capitals
 # Create your views here.
 
+
+
+
 def capital_list(request):
+    states= Capitals.objects.all()
     capitals = Capitals.objects.all()
-    return render(request, 'usacapitals/capital_list.html', {'capitals':capitals})
+    return render(request, 'usacapitals/capital_list.html', {'capitals':capitals,'states':states})
